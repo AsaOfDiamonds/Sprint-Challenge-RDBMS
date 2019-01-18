9d599a85c05bd79ae9cbcfa16a693fa4d5a32c16
1@$@1github <asaofdiamonds@gmail.com>
2019-01-18 11:25:21 -0700

    fixed bug, can now get projects and actions

---
+++ helpers/index.js
@@ -0,0 +1,21 @@
+const db = require('../dbConfig');
+
+
+
+module.exports = {
+    getProjects() {
+        return db('projects');
+    },
+
+    addProject(project) {
+        return db('projects').insert(project);
+    },
+
+    getActions() {
+        return db('actions');
+    },
+    
+    addAction(action) {
+        return db('actions').insert(action);
+    }
+};
--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const configureMiddleware = require('./config/middleware');
 
+const helpers = require('./helpers/index');
+
 const server = express();
 
 //middleware 
 configureMiddleware(server);
 
-//routes 
+//routes end points
 
 server.get('/', (req, res) => {
     res.send('And God saw all that He had made, and found it very good');
 });
 
-module.exports = server 
+server.get('/projects', function (req, res) {
+    helpers
+        .getProjects()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(500).json({ message: ` Failed to get Projects`, error: err });
+    });
+});
+
+server.get('/actions', function (req, res) {
+    helpers
+        .getActions()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(500).json({ message: ` Failed to get Actions`, error: err });
+        });
+});
+
+
+module.exports = server
